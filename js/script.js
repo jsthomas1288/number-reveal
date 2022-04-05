b16debb0ca31f445c2398718d3a4045adce60142
Jacob Thomas <98564375+jsthomas1288@users.noreply.github.com>
2022-04-05 11:23:52 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var clearButton = document.querySelector("button");
+
+document.addEventListener("keydown", function (e) {
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "💪";
+  } else if (key === "2") {
+    box2.innerText = "😂";
+  } else {
+    box3.innerText = "🔥";
+  }
+  clearButton.addEventListener("click", function () {
+    box1.innerText = "1";
+    box2.innerText = "2";
+    box3.innerText = "3";
+  });
+});
